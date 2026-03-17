4b3caedf73c31babc706679214fc42c38329b2fa
Shashank Singh <sshingh456@gmail.com>
2026-03-17 15:18:07 +0530

    Add implementation for k rotations of an array and maximum profit calculation from stock prices

---
+++ array/practice/kRotations.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
+    int k = 2;
+    for(int i=0; i<k; i++) {
+        int first = arr[0];
+        for(int j=0; j<n; j++) {
+            arr[j] = arr[j+1];
+        }
+        arr[n-1] = first;
+    }
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
