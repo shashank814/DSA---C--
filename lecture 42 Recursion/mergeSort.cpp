36befe4126d8de66971366852581ba3f8f31921a
Shashank Singh <sshingh456@gmail.com>
2026-04-11 11:46:30 +0530

    Refactor merge sort implementation to use vector and improve memory management; add palindrome partitioning algorithm

--- lecture 42 Recursion/mergeSort.cpp
+++ lecture 42 Recursion/mergeSort.cpp
@@ -1,71 +1,55 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void merge(int *arr, int s, int e) {
-    int mid = (s+e)/2;
+void merge(vector<int> &arr, int st, int mid, int end) {
+    vector<int> temp;
+    int i = st, j = mid + 1;
 
-    int len1 = mid - s + 1;
-    int len2 = e - mid;
-
-    int *first = new int[len1];
-    int *second = new int[len2];
-
-    int mainArrayIndex = s;
-    for(int i=0; i<len1; i++) {
-        first[i] = arr[mainArrayIndex++];
-    }
-
-    mainArrayIndex = mid + 1;
-    for(int i=0; i<len2; i++) {
-        second[i] = arr[mainArrayIndex++];
-    }
-
-    // merge 2 sorted arrays
-    int index1 = 0; 
-    int index2 = 0;
-    mainArrayIndex = s;
-
-    while(index1 < len1 && index2 < len2) {
-        if(first[index1] < second[index2]) {
-            arr[mainArrayIndex++] = first[index1++];
+    while(i <= mid && j <= end) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
         } else {
-            arr[mainArrayIndex++] = second[index2++];
+            temp.push_back(arr[j]);
+            j++;
         }
     }
 
-    while (index1 < len1)
-    {
-        arr[mainArrayIndex++] = first[index1++];
+    while(i <= mid) {
+        temp.push_back(arr[i]);
+        i++;
     }
 
-    while (index2 < len2) 
-    {
-        arr[mainArrayIndex++] = second[index2++];
+    while(j <= end) {
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for(int idx=0; idx<temp.size(); idx++) {
+        arr[idx+st] = temp[idx];
     }
 }
 
-void mergeSort(int *arr, int s, int e) {
-    if(s >= e) {
-        return;
+void mergeSort(vector<int> &arr, int st, int end) {
+    if(st < end) {
+        int mid = st + (end - st)/2;
+
+        mergeSort(arr, st, mid);
+        mergeSort(arr, mid+1, end);
+
+        merge(arr, st, mid, end);
     }
-
-    int mid = s + (e-s)/2;
-
-    mergeSort(arr, s, mid);
-
-    mergeSort(arr, mid+1, e);
-
-    merge(arr, s, e);
 }
 
 int main() {
-    int arr[5] = {2,5,1,6,9};
+    vector<int> arr = {2,5,1,6,9};
     int n = 5;
 
-    mergeSort(arr, 0, n-1);
+    mergeSort(arr, 0, arr.size()-1);
 
-    for(int i=0; i<n; i++) {
-        cout << arr[i] << " ";
+    for(int val : arr) {
+        cout << val << " ";
     }
 
     return 0;
