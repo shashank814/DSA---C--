a366b2f6849ad8dfaac12cf9b3bd1bfcfddfcbe6
Shashank Singh <sshingh456@gmail.com>
2026-04-20 12:11:57 +0530

    Add implementation for a doubly linked list with push, pop, and print functions

---
+++ lecture 63 Doubly Linked List/doublyLinkedList.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+       data = val;
+       next = prev = NULL;
+    }
+};
+
+class DoublyList {
+    Node* head;
+    Node* tail;
+
+    public:
+    DoublyList() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if(tail == NULL) {
+            head = tail = NULL;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front() {
+        if(head == NULL) {
+            cout << "DLL is emoty\n";
+            return;
+        }
+        Node* temp = head;
+        head = head->next;
+        if(head != NULL) {
+            head->prev = NULL;
+        }
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back() {
+        if(tail == NULL) {
+            cout << "Tail is empty\n";
+            return;
+        }
+        Node* temp = tail;
+        tail = tail->prev;
+        if(tail != NULL) {
+            tail->next = NULL;
+        }
+        temp->prev = NULL;
+        delete temp;
+    }
+
+    void print() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout << temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main() {
+    DoublyList dll;
+
+    dll.push_front(1);
+    dll.push_front(2);
+    dll.push_front(3);
+
+    dll.push_back(4);
+    dll.push_back(5);
+
+    dll.pop_front();
+    dll.pop_back();
+
+    dll.print();
+
+    return 0;
+}
