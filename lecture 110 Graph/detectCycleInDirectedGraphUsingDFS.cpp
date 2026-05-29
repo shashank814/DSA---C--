42d36788d2d73d50d5fb357cc9e06e03fc19ded0
Shashank Singh <sshingh456@gmail.com>
2026-05-29 12:44:51 +0530

    Add implementation for cycle detection in directed graph using DFS

---
+++ lecture 110 Graph/detectCycleInDirectedGraphUsingDFS.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+#include<stack>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *l;
+
+    public:
+    Graph(int V) {
+        this->V = V;
+        l = new list<int> [V];
+    }
+
+    void addEdge(int u, int v) {
+        l[u].push_back(v);
+    }
+
+    // cycle detection
+    bool isCycleDirDFS(int curr, vector<bool> &vis, vector<bool> &recPath) {
+        vis[curr] = true;
+        recPath[curr] = true;
+
+        for(int v : l[curr]) {
+            if(!vis[v]) {
+                if(isCycleDirDFS(v, vis, recPath)) {
+                    return true;
+                }
+            } else if(recPath[v]) {
+                return true;
+            }
+        }
+
+        recPath[curr] = false;
+        return false;
+    }
+
+    bool isCycle() {
+        vector<bool> vis(V, false);
+        vector<bool> recPath(V, false);
+
+        for(int i=0; i<V; i++) {
+            if(!vis[i]) {
+                if(isCycleDirDFS(i, vis, recPath)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Graph g(4);
+
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 0);
+
+    cout << g.isCycle() << endl;
+
+    return 0;
+}
